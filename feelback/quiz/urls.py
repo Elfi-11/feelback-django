06349327cc022a25ef0d43a1c1cfaa0ce6a44e18
noarobert <noarobertpro@gmail.com>
2024-10-03 10:54:20 +0200

    front

--- feelback/quiz/urls.py
+++ feelback/quiz/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("form/", views.feedbackForm, name="form"),
+    path("dashboard/", views.dashboard, name="dashboard"),
 ]
